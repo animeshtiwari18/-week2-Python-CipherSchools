c1bda302a1c005c74a1b716ca1926d65f93c3fb7
animeshtiwari18 <119746686+animeshtiwari18@users.noreply.github.com>
2022-12-14 22:12:33 +0530

    Add files via upload

---
+++ Class 120-Update Dictionary -CipherSchools.py
@@ -0,0 +1,12 @@
+# update method
+
+user_info={
+    'name':'Shivam',
+    'age': 19,
+    'fav_movies': ['coco'],
+    'fav_tunes': ['awakening','fairy tale'],
+}
+
+more_info= {'state' : 'bihar', 'hobbies' : ['coding','reading','guiter']}
+user_info.update({})
+print(user_info)
---
+++ Class 122-More about get() Method-CipherSchools.py
@@ -0,0 +1,4 @@
+# more about get method
+user= {'name' : 'Shivam', 'age' : '19', 'age' : '29'}
+# print(user.get('names','not found'))
+print(user)
